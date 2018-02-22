761c82042bf2099e4b5b567e26bf72a2be50980f
Unknown <20hussan@elmbrookstudents.org>
2018-02-22 16:04:32 -0600

    Added stuff

--- src/personStuff/Person.java
+++ src/personStuff/Person.java
@@ -3,13 +3,24 @@ package personStuff;
 public class Person {
 	private String name;
 	private double salary;
+	private Occupation job;
+	private Address place;
+	private InterestingFacts boring;
+	private Projects cool;
+	private Schedule wierd;
 
 	public Person() {
 		name = "Dave Ryan";
 		salary = 80282.0;
+		job = new Occupation();
+		place = new Address();
+		boring = new InterestingFacts();
+		cool = new Projects();
+		wierd = new Schedule();
 	}
 
 	public void moreMoney() {
 		salary += 1000.0;
 	}
+	
 }
